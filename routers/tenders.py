c4b7d5643f876ca3ee6d90b375bb09055ace1f42
derek <adamtsai0408@gmail.com>
2026-03-05 13:34:01 +0800

    [修改] 修改爬蟲邏輯(手動爬從將資料存回cached_data)

--- routers/tenders.py
+++ routers/tenders.py
@@ -3,23 +3,25 @@ from typing import List, Optional
 from models import TenderItem,TenderCacheResponse
 from scraper import Scraper
 from shared.cache import cached_data
+from datetime import datetime
+
 import logging
 
 router = APIRouter(prefix="/api/tenders", tags=["tenders"])
 logger = logging.getLogger(__name__)
 
 
-@router.get("/", response_model=List[TenderItem])
+@router.get("/", response_model=TenderCacheResponse)
 async def get_tenders(tenderName: Optional[str] = None):
     scraper = Scraper(headless=True)
     # 初始化 scraper 建立新的物件 
     # headless=True: 是否以無頭模式運行 程式會在背景執行瀏覽器 不會顯示
     # 爬完資料後關閉瀏覽器
     try:
-        data = scraper.scrape_data(keyword=tenderName)
-        # 也可以寫成
-        # data = scraper.scrape_data(tenderName) 但為了可讀性 需要keyword = tenderName
-        return data
+        result = scraper.scrape_data(keyword=tenderName)
+        cached_data["data"] = result
+        cached_data["last_updated"] = str(datetime.now())
+        return cached_data
     except Exception as e:
         logger.error(f"Scraping failed: {e}")
         raise HTTPException(status_code=500, detail=str(e))
